c477dac6a2556616638542386a127ff3a9e8bda5
Giảng Thanh Danh <44111969+GTDANH@users.noreply.github.com>
2023-03-20 15:44:08 +0700

    Reprogramme

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: { 'react/jsx-uses-react': 'error', 'react/jsx-uses-vars': 'error' },
+};
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import {message, Menu, Dropdown} from "antd"
+
+const Header = () => {
+  return <div className="header">
+    
+  </div>;
+};
+
+export default Header;
